a49d4d104f9bd378f69488f9658b1b02d0444263
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-03-30 20:21:12 -0700

    Edit app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/styles/theme";
+import Sidenav from "./../components/sidebar/Sidenav";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import Topbar from "./../components/topbar/topbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ProSidebarProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidenav />
+          <div className="content">
+            <Topbar />
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </ThemeProvider>
+    </ProSidebarProvider>
+  );
 }
