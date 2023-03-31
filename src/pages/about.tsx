fa006be84738b10293999b7b7fd5c69bf7bf485b
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-03-30 20:21:04 -0700

    Add about, calendar and dashboard pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+
+const About = () => {
+  return (
+    <Box>
+      <h1>About</h1>
+    </Box>
+  );
+};
+
+export default About;
---
+++ src/pages/calendar.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Calendar = () => {
+  return <Box>Calendar</Box>;
+};
+
+export default Calendar;
