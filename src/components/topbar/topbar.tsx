2c99ec7c5c152f33cff394899b334429d67c83ae
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-04-02 10:20:48 -0700

    Add dark and light mode toggle button

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -8,11 +8,12 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import { ColorModeContext, tokens } from "@/styles/theme";
 
 const Topbar = () => {
   const theme = useTheme();
-  // const colors = tokens(theme.palette.mode);
-  // const colorMode = useContext(ColorModeContext);
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
@@ -30,13 +31,13 @@ const Topbar = () => {
 
       {/* ICONS */}
       <Box display="flex">
-        {/* <IconButton onClick={colorMode.toggleColorMode}>
+        <IconButton onClick={() => colorMode.toggleColorMode()}>
           {theme.palette.mode === "dark" ? (
             <DarkModeOutlinedIcon />
           ) : (
             <LightModeOutlinedIcon />
           )}
-        </IconButton> */}
+        </IconButton>
         <IconButton>
           <NotificationsOutlinedIcon />
         </IconButton>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,28 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "@/styles/theme";
 import Sidenav from "./../components/sidebar/Sidenav";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import Topbar from "./../components/topbar/topbar";
+import { ColorModeContext, useMode } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { theme, colorMode } = useMode();
+
   return (
-    <ProSidebarProvider>
+    <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <Sidenav />
-          <div className="content">
-            <Topbar />
-            <Component {...pageProps} />
+        <ProSidebarProvider>
+          <CssBaseline />
+          <div className="app">
+            <Sidenav />
+            <div className="content">
+              <Topbar />
+              <Component {...pageProps} />
+            </div>
           </div>
-        </div>
+        </ProSidebarProvider>
       </ThemeProvider>
-    </ProSidebarProvider>
+    </ColorModeContext.Provider>
   );
 }
