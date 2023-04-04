7db7cabf396f0ca339ee4866e5ec8dbb3a8d152e
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-04-03 19:28:02 -0700

    Add prompt on event click

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,24 +1,22 @@
-import Box from "@mui/material/Box";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import { useState } from "react";
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 
 const Calendar = () => {
-  const [events, setEvents] = useState([
+  const [events, setEvents] = React.useState([
     { title: "event 1", date: "2023-04-01" },
     { title: "event 2", date: "2023-04-02" },
   ]);
   const handleDateClick = () => {
-    alert("clicked");
+    // bind with an arrow function
+    const title = prompt("Add a new event");
   };
-
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
+      plugins={[dayGridPlugin, interactionPlugin]}
+      dateClick={handleDateClick}
       events={events}
-      eventClick={handleDateClick}
-      select={handleDateClick}
     />
   );
 };
