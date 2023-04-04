e8df21aa6b87a1d883c378c6090bca425a52ac3e
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-04-03 19:03:11 -0700

    Add calendar

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,7 +1,26 @@
-import { Box } from "@mui/material";
+import Box from "@mui/material/Box";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { useState } from "react";
 
 const Calendar = () => {
-  return <Box>Calendar</Box>;
+  const [events, setEvents] = useState([
+    { title: "event 1", date: "2023-04-01" },
+    { title: "event 2", date: "2023-04-02" },
+  ]);
+  const handleDateClick = () => {
+    alert("clicked");
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventClick={handleDateClick}
+      select={handleDateClick}
+    />
+  );
 };
 
 export default Calendar;
