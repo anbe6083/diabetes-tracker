a09cae572c61906287d81b1377caebb48dcbd068
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-04-04 08:26:33 -0700

    Add timegrid to calendar

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -2,23 +2,34 @@ import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 const Calendar = () => {
   const [events, setEvents] = React.useState([
-    { title: "event 1", date: new Date("2023-04-01") },
-    { title: "event 2", date: new Date("2023-04-02") },
+    { title: "event 1", date: new Date("2023-04-01"), duration: "120" },
+    { title: "event 2", date: new Date("2023-04-02"), duration: "120" },
   ]);
-  const handleDateClick = (arg: any) => {
-    // bind with an arrow function
+  const handleDateClick = (selected: any) => {
+    console.log(selected);
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
     const title = prompt("Add a new event");
     if (title) {
-      setEvents([...events, { title, date: arg.dateStr }]);
+      setEvents([...events, { title, date: selected.dateStr, duration: "60" }]);
     }
   };
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
+      plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
       dateClick={handleDateClick}
+      initialView="timeGridDay"
+      headerToolbar={{
+        left: "prev,next,today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+      }}
+      slotDuration="00:30:00"
+      slotLabelInterval="01:00:00"
       events={events}
     />
   );
