cf036bb857f439a97b580b7cb113ab3bf6651e55
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-04-10 18:46:51 -0700

    Refactor EditModal into separate component

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -71,6 +71,9 @@ const Calendar = () => {
         backgroundColor: background,
       },
     ]);
+    setInsulinValue("0");
+    setGlucoseValue("0");
+    setMealDescription("");
     setOpen(false);
     setDate(new Date());
   };
@@ -98,6 +101,7 @@ const Calendar = () => {
   };
 
   const handleEventClick = (e: any) => {
+    setOpen(true);
     const eventObj = e.event;
     console.log(eventObj);
   };
@@ -112,6 +116,94 @@ const Calendar = () => {
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
+  };
+
+  const EditModal = () => {
+    return (
+      <Modal
+        open={open}
+        onClose={handleSubmit}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box minWidth={120}>
+            <InputLabel id="title-select-label">Title</InputLabel>
+            <Select
+              labelId="title-select-label"
+              id="title-select"
+              value={title}
+              label="Title"
+              onChange={handleTitleChange}
+            >
+              <MenuItem value="Glucose Reading">Glucose Reading</MenuItem>
+              <MenuItem value="Insulin Injection">Insulin Injection</MenuItem>
+              <MenuItem value="Meal">Meal</MenuItem>
+            </Select>
+            {title === "Glucose Reading" && (
+              <Box>
+                <Typography
+                  id="modal-modal-title"
+                  variant="h6"
+                  component="h2"
+                ></Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2, mb: 8 }}>
+                  <TextField
+                    id="outlined-basic"
+                    label="mg/dL"
+                    variant="outlined"
+                    type="number"
+                    value={glucoseValue}
+                    onChange={(e) => setGlucoseValue(e.target.value)}
+                  />
+                </Typography>
+              </Box>
+            )}
+            {title === "Insulin Injection" && (
+              <Box>
+                <Typography
+                  id="modal-modal-title"
+                  variant="h6"
+                  component="h2"
+                ></Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2, mb: 8 }}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Units"
+                    variant="outlined"
+                    type="number"
+                    value={insulinValue}
+                    onChange={(e) => setInsulinValue(e.target.value)}
+                  />
+                </Typography>
+              </Box>
+            )}
+            {title === "Meal" && (
+              <Box>
+                <Typography
+                  id="modal-modal-title"
+                  variant="h6"
+                  component="h2"
+                ></Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2, mb: 8 }}>
+                  <TextField
+                    id="outlined-basic"
+                    label="What did your pet eat?"
+                    variant="outlined"
+                    type="text"
+                    value={mealDescription}
+                    onChange={(e) => setMealDescription(e.target.value)}
+                  />
+                </Typography>
+              </Box>
+            )}
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    );
   };
 
   return (
@@ -138,98 +230,7 @@ const Calendar = () => {
             handleEventClick(e);
           }}
         />
-        <Modal
-          open={open}
-          onClose={handleSubmit}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Box minWidth={120}>
-              <InputLabel id="title-select-label">Title</InputLabel>
-              <Select
-                labelId="title-select-label"
-                id="title-select"
-                value={title}
-                label="Title"
-                onChange={handleTitleChange}
-              >
-                <MenuItem value="Glucose Reading">Glucose Reading</MenuItem>
-                <MenuItem value="Insulin Injection">Insulin Injection</MenuItem>
-                <MenuItem value="Meal">Meal</MenuItem>
-              </Select>
-              {title === "Glucose Reading" && (
-                <Box>
-                  <Typography
-                    id="modal-modal-title"
-                    variant="h6"
-                    component="h2"
-                  ></Typography>
-                  <Typography
-                    id="modal-modal-description"
-                    sx={{ mt: 2, mb: 8 }}
-                  >
-                    <TextField
-                      id="outlined-basic"
-                      label="mg/dL"
-                      variant="outlined"
-                      type="number"
-                      value={glucoseValue}
-                      onChange={(e) => setGlucoseValue(e.target.value)}
-                    />
-                  </Typography>
-                </Box>
-              )}
-              {title === "Insulin Injection" && (
-                <Box>
-                  <Typography
-                    id="modal-modal-title"
-                    variant="h6"
-                    component="h2"
-                  ></Typography>
-                  <Typography
-                    id="modal-modal-description"
-                    sx={{ mt: 2, mb: 8 }}
-                  >
-                    <TextField
-                      id="outlined-basic"
-                      label="Units"
-                      variant="outlined"
-                      type="number"
-                      value={insulinValue}
-                      onChange={(e) => setInsulinValue(e.target.value)}
-                    />
-                  </Typography>
-                </Box>
-              )}
-              {title === "Meal" && (
-                <Box>
-                  <Typography
-                    id="modal-modal-title"
-                    variant="h6"
-                    component="h2"
-                  ></Typography>
-                  <Typography
-                    id="modal-modal-description"
-                    sx={{ mt: 2, mb: 8 }}
-                  >
-                    <TextField
-                      id="outlined-basic"
-                      label="What did your pet eat?"
-                      variant="outlined"
-                      type="text"
-                      value={mealDescription}
-                      onChange={(e) => setMealDescription(e.target.value)}
-                    />
-                  </Typography>
-                </Box>
-              )}
-              <Button variant="contained" onClick={handleSubmit}>
-                Submit
-              </Button>
-            </Box>
-          </Box>
-        </Modal>
+        <EditModal />
       </Box>
     </LocalizationProvider>
   );
