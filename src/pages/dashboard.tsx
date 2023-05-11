5ab6c4d6e17941559e61a5e43e71714698591d3e
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-05-11 08:41:47 -0700

    Add dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,21 @@
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
 const Dashboard = () => {
-  return <Box>Dashboard</Box>;
+  return (
+    <Container>
+      <Box display={"flex"} flexDirection={"row"}>
+        <Box sx={{ backgroundColor: "red" }}>Box 1</Box>
+        <Box sx={{ backgroundColor: "blue" }}>Box 2</Box>
+        <Box sx={{ backgroundColor: "green" }}>Box 3</Box>
+      </Box>
+      <Box display={"flex"} flexDirection={"row"}>
+        <Box sx={{ backgroundColor: "red" }}>Box 1</Box>
+        <Box sx={{ backgroundColor: "blue" }}>Box 2</Box>
+        <Box sx={{ backgroundColor: "green" }}>Box 3</Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Dashboard;
