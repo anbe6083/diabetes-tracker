170df886a6f6ee54fcb95c682d9674d1987dcf4e
Andrew Berumen <10716752+anbe6083@users.noreply.github.com>
2023-05-24 11:01:45 -0700

    Add modal for adding insulin

---
+++ src/components/modals/AddInsulinModal.tsx
@@ -0,0 +1,114 @@
+import { tokens } from "@/styles/theme";
+import dayjs from "dayjs";
+import {
+  Modal,
+  Box,
+  InputLabel,
+  Select,
+  Typography,
+  TextField,
+  Button,
+  SelectChangeEvent,
+  useTheme,
+} from "@mui/material";
+import { SyntheticEvent, useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Dayjs } from "dayjs";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+type AddInsulinProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  setInsulinData: (events: any) => void;
+  insulin: number;
+  date: Dayjs | null;
+  setDate: React.Dispatch<React.SetStateAction<Dayjs | null>>;
+  insulinData: any;
+  insulinValue: string;
+  setInsulinValue: (insulinValue: string) => void;
+};
+
+export const AddInsulinModal = ({
+  open,
+  setOpen,
+  setDate,
+  setInsulinData,
+  insulinData,
+  insulinValue,
+  setInsulinValue,
+  date,
+}: AddInsulinProps) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  type BackdropClickEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
+
+  const handleSubmit = (event: BackdropClickEvent) => {
+    if (event.target === event.currentTarget) {
+      setOpen(false);
+      setInsulinValue("0");
+      return;
+    }
+    setDate(dayjs());
+
+    const newDataPoint = {
+      x: date?.format("YYYY-MM-DD").toString(),
+      y: insulinValue,
+    };
+    const updatedData = [...insulinData];
+    updatedData[0].data.push(newDataPoint);
+    setInsulinData(updatedData);
+    setInsulinValue("0");
+    setOpen(false);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Modal
+        open={open}
+        onClose={handleSubmit}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box minWidth={120}>
+            <Box>
+              <Typography
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+              ></Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2, mb: 8 }}>
+                <TextField
+                  id="outlined-basic"
+                  label="Units"
+                  variant="outlined"
+                  type="number"
+                  value={insulinValue}
+                  onChange={(e) => setInsulinValue(e.target.value)}
+                />
+              </Typography>
+              <DatePicker onChange={(e) => setDate(e)} />
+            </Box>
+            <div onClick={handleSubmit}>
+              <Button variant="contained">Submit</Button>
+            </div>
+          </Box>
+        </Box>
+      </Modal>
+    </LocalizationProvider>
+  );
+};
--- src/pages/curve.tsx
+++ src/pages/curve.tsx
@@ -1,29 +1,71 @@
 import Box from "@mui/material/Box";
 import LineChart from "./../components/charts/lineCharts";
 import {
-  mockLineDataInsulin as insulinData,
-  mockLineDataBloodSugar as sugarData,
+  mockLineDataInsulin,
+  mockLineDataBloodSugar,
 } from "./../data/mockData";
 import { ColorModeContext, tokens } from "@/styles/theme";
 import { useTheme } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Button from "@mui/material/Button";
+import { EditModal } from "@/components/modals/EditModal";
+import { AddInsulinModal } from "@/components/modals/AddInsulinModal";
+import { Dayjs } from "dayjs";
 
 const BGCurve = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+  const [events, setEvents] = useState([]);
+  const [insulinData, setInsulinData] = useState(mockLineDataInsulin);
+  const [bloodSugarData, setBloodSugarData] = useState(mockLineDataBloodSugar);
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<Dayjs | null>(null);
+  const [insulinValue, setInsulinValue] = useState("0");
+
+  const handleOpen = () => {
+    setOpen(true);
+    // const newDataPoint = {
+    //   x: date.toString(),
+    //   y: 0,
+    // };
+    // const updatedData = [...insulinData];
+    // updatedData[0].data.push(newDataPoint);
+    // setInsulinData(updatedData);
+  };
 
   return (
     <Box>
-      <LineChart
-        data={insulinData}
-        yUnits={"Units"}
-        colors={colors.blueAccent[400]}
-      />
-      <LineChart
-        data={sugarData}
-        yUnits={"mg/dL"}
-        colors={colors.greenAccent[400]}
+      <Box>
+        <Button variant="contained" onClick={handleOpen}>
+          Add Insulin
+        </Button>
+        <LineChart
+          data={insulinData}
+          yUnits={"Units"}
+          colors={colors.blueAccent[400]}
+        />
+      </Box>
+      <Box>
+        <Button variant="contained" onClick={handleOpen}>
+          Add Glucose
+        </Button>
+        <LineChart
+          data={bloodSugarData}
+          yUnits={"mg/dL"}
+          colors={colors.greenAccent[400]}
+        />
+      </Box>
+      <AddInsulinModal
+        setInsulinData={setInsulinData}
+        insulinData={insulinData}
+        insulin={100}
+        open={open}
+        date={date}
+        setOpen={setOpen}
+        setDate={setDate}
+        insulinValue={insulinValue}
+        setInsulinValue={setInsulinValue}
       />
     </Box>
   );
